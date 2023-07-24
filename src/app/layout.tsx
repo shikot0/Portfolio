6e77dde05b1140aeb70c38d1696c635e9d2ffa6d
shikoto <1sh1m1da1@gmail.com>
2023-07-24 05:04:28 +0000

    be deadass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+import {Analytics} from '@vercel/analytics/react'
 
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      {process.env.NODE_ENV === 'production' ? <Analytics /> : null}
     </html>
   )
 }
